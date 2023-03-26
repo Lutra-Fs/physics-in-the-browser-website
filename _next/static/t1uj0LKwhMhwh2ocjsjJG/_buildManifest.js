57e53b20edc65468b57f324cbc42d9ec50a14987
Lutra-Fs <36790218+Lutra-Fs@users.noreply.github.com>
2023-03-26 10:27:59 +0000

    Deploying to gh-pages from @ techlauncher-mlai-edge-physics/techlauncher-mlai-edge-physics.github.io@563961cbfd75a714c274c60b2d3c59a50de41b50 🚀

---
+++ _next/static/t1uj0LKwhMhwh2ocjsjJG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/50901216b76e581e.css","static/chunks/pages/index-327595e5809909bb.js"],"/_error":["static/chunks/pages/_error-3986dd5834f581dc.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
